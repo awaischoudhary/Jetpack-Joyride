a714049e99a2aa8094bcf51e123a88fa3a82958e
Awais Choudhary <awaischoudhary@Awaiss-Air.cogeco.local>
2021-01-15 19:46:57 -0500

    initial game

--- src/awaisfinalgame/AwaisFinalGameController.java
+++ src/awaisfinalgame/AwaisFinalGameController.java
@@ -1,5 +1,33 @@
 package awaisfinalgame;
 
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 public class AwaisFinalGameController {
+
+	// setting the canvas
+	@FXML
+	Canvas gameCanvas;
 	
+	// setting the score
+	@FXML 
+	Label displayScore;
+
+	// setting the graphics context, the gameScene, and stage
+	GraphicsContext gc;
+	Scene gameScene;
+	Stage stage;
+	
+	public void setScene(Stage stage) {
+		gameScene = stage.getScene();
+	}
+	
+	public void gameLoop() {
+		System.out.println("Hello");
+		
+	}
 }
--- src/awaisfinalgame/Main.java
+++ src/awaisfinalgame/Main.java
@@ -1,26 +1,30 @@
 package awaisfinalgame;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
-
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("AwaisFinalGame.fxml"));
-			Scene scene = new Scene(root,400,400);
+			primaryStage.setTitle("Jetpack Joyride");
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("AwaisFinalGame.fxml"));
+			BorderPane root = (BorderPane) loader.load();
+			Scene scene = new Scene(root, 600, 300);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			AwaisFinalGameController controller = loader.getController();
 			primaryStage.setScene(scene);
+			controller.setScene(primaryStage);
+			controller.gameLoop();
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
